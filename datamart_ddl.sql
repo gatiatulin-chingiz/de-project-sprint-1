aa7c1b08a1503d654a71bf1a975aa567853db263
mller <gatiatulin.chingiz@gmail.com>
2023-12-11 08:12:46 +0300

    revue_1

--- datamart_ddl.sql
+++ datamart_ddl.sql
@@ -3,4 +3,9 @@ CREATE TABLE analysis.dm_rfm_segments (
     recency int4 NOT NULL,
     frequency int4 NOT NULL,
     monetary_value int4 NOT NULL
-)
+);
+
+ALTER TABLE dm_rfm_segments
+ADD CONSTRAINT dm_rfm_segments_recency_check CHECK (((recency >= 1) AND (recency <= 5))),
+ADD CONSTRAINT dm_rfm_segments_frequency_check CHECK (((frequency >= 1) AND (frequency <= 5))),
+ADD CONSTRAINT dm_rfm_segments_monetary_value_check CHECK (((monetary_value >= 1) AND (monetary_value <= 5)))
--- datamart_query.sql
+++ datamart_query.sql
@@ -20,7 +20,9 @@ frequency AS (
         SELECT o.user_id,
                COUNT(o.order_id) AS order_count
         FROM analysis.orders AS o
+        LEFT JOIN analysis.orderstatuses AS os ON o.status=os.id
         WHERE o.order_ts >= '2022-01-01'
+          AND os.key = 'Closed'
         GROUP BY o.user_id
         ORDER BY user_id
     ) as a
@@ -44,20 +46,20 @@ SELECT r.user_id,
        f.frequency,
        mv.monetary_value
 FROM recency AS r
-LEFT JOIN frequency AS f ON r.user_id=f.user_id
-LEFT JOIN monetary_value AS mv ON r.user_id=mv.user_id
+JOIN frequency AS f ON r.user_id=f.user_id
+JOIN monetary_value AS mv ON r.user_id=mv.user_id
 ORDER BY r.user_id
 
 
 
 user_id recency frequency monetary_value
-0	1	5	  4
-1	4	3	  3
-2	2	3	  5
-3	2	3	  3
-4	4	3	  3
-5	5	5	  5
-6	1	2	  5
-7	4	1	  2
-8	1	1	  3
-9	1	3	  2
+0   1   3   4
+1   4   3   3
+2   2   3   5
+3   2   3   3
+4   4   3   3
+5   5   5   5
+6   1   3   5
+7   4   2   2
+8   1   1   3
+9   1   2   2
